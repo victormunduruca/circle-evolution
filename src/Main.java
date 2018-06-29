01c17789c1d762ecd275db7e0ce06e602105562c
Victor Munduruca <victormunduruca@gmail.com>
2018-06-29 11:13:15 -0300

    Simple parent selection

--- src/Main.java
+++ src/Main.java
@@ -3,9 +3,14 @@ import java.awt.Color;
 import java.awt.Graphics2D;
 import java.awt.image.BufferedImage;
 import java.awt.image.DataBufferByte;
+import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.concurrent.ThreadLocalRandom;
+
+import javax.imageio.ImageIO;
 
 import model.Circle;
 import model.Individual;
@@ -54,26 +59,43 @@ public class Main {
 //			File outputfile = new File("saved"+ o+ ".png");
 //			ImageIO.write(img, "png", outputfile);
 //		}
-//		originalImg = ImageIO.read(new File("dog.jpg"));
+		originalImg = ImageIO.read(new File("dog.jpg"));
 //		BufferedImage img = ImageIO.read(new File("dog copy.jpg"));
 //		System.out.println(fitness(img));
 		
+		//Mutation test
+//		initializePopulation();
+//		printIndividual(population.get(0));
+//		printIndividual(population.get(1));
+//		mutation(0);
+//		mutation(1);
+//		System.out.println("Crossover");
+//		printIndividual(population.get(0));
+//		printIndividual(population.get(1));
+		
+		//Parent Selection Test
 		initializePopulation();
-		printIndividual(population.get(0));
-		printIndividual(population.get(1));
-		mutation(0);
-		mutation(1);
-		System.out.println("Crossover");
-		printIndividual(population.get(0));
-		printIndividual(population.get(1));
+		
+		for(Individual i: population) {
+			System.out.println("Fitness: " +i.getFitness() + "");
+		}
+		ArrayList<Individual> best= parentSelection();
+		System.out.println();
+		for(Individual i: population) {
+			System.out.println("Fitness: " +i.getFitness() + "");
+		}
+		System.out.println("Best1: " +best.get(0).getFitness());
+		System.out.println("Best2: " +best.get(1).getFitness());
+		
 	}
 	public static void initializePopulation() {
 		population = new ArrayList<Individual>();
-		for (int i = 0; i < 2; i++) { 
-			Individual newIndividual = new Individual(); //create new indvidual
+		for (int i = 0; i < 10; i++) { 
+			Individual newIndividual = new Individual(); //create new individual
 			newIndividual.addRandomly(chromossomeSize, imgWidth, imgHeight); //add random circles to the individual
 			population.add(newIndividual); //add individual to population
 		}
+		calculatePopulationFitness();
 	}
 	public static BufferedImage paintIndividual(Individual i) {
 		BufferedImage image = new BufferedImage(imgWidth, imgHeight, BufferedImage.TYPE_BYTE_GRAY); //create blank image
@@ -84,16 +106,7 @@ public class Main {
 		}
 		return image; //returns the painted image
 	}
-	public static int fitness(BufferedImage img) {
-		int fitness = 0, subtraction;
-		byte[] imgPixels = ((DataBufferByte) img.getRaster().getDataBuffer()).getData(); //get pixel arrays
-		byte[] originalPixels = ((DataBufferByte) originalImg.getRaster().getDataBuffer()).getData();
-		for(int i = 0; i < imgPixels.length; i++) { //iterate over arrays and subtract equivalent pixels and sum the 
-			subtraction = Math.abs(imgPixels[i] - originalPixels[i]);//get the absolute value of the subtraction 
-			fitness += subtraction;//keep the result into a variable 
-		}
-		return fitness/imgPixels.length;
-	}
+	
 	public static void crossover(int firstIndex, int secondIndex) {
 		ArrayList<Circle> firstChromossome = population.get(firstIndex).getChromossome(); //retrieve first and second chromossomes
 		ArrayList<Circle> secondChromossome = population.get(secondIndex).getChromossome();
@@ -111,7 +124,7 @@ public class Main {
 		ArrayList<Circle> chromossome = population.get(index).getChromossome();  //get chromossome
 		int randomIndex = ThreadLocalRandom.current().nextInt(0, chromossomeSize);//select a random gene
 		int randomFeature = ThreadLocalRandom.current().nextInt(0, 3);//select a random characteristic
-		//randomly reset its value
+		//randomly reset the choosen characteristic's value
 		if(randomFeature == 0) {
 			chromossome.get(randomIndex).setGrayScale(ThreadLocalRandom.current().nextInt(0, 256)); 
 		} else if (randomFeature == 1){
@@ -120,7 +133,37 @@ public class Main {
 			chromossome.get(randomIndex).setX(ThreadLocalRandom.current().nextInt(0, imgWidth));
 			chromossome.get(randomIndex).setY(ThreadLocalRandom.current().nextInt(0, imgHeight));
 		}
-		
+	}
+	public static ArrayList<Individual> parentSelection() {
+		//TODo
+		sortPopulation();
+		ArrayList<Individual> bestIndividuals = new ArrayList<Individual>();
+		bestIndividuals.add(population.get(0));
+		bestIndividuals.add(population.get(1));
+		return bestIndividuals;
+	}
+	public static void sortPopulation() {
+		Collections.sort(population, new Comparator<Individual>() {
+			@Override
+			public int compare(Individual o1, Individual o2) {
+				Individual ind1 = (Individual) o1;
+				Individual ind2 = (Individual) o2;
+				if(ind1.getFitness() < ind2.getFitness()) 
+					return -1;
+				else if(ind1.getFitness() == ind2.getFitness()) 
+					return 0;
+				else 
+					return 1;
+			}
+		});
+	}
+	public static void calculatePopulationFitness() {
+		int index = 0;
+		for(Individual i : population) {
+			BufferedImage img = paintIndividual(population.get(index));
+			i.setFitness(img, originalImg);
+			index++;
+		}
 	}
 	public static void printIndividual(Individual i) {
 		//TODO remover
--- src/model/Individual.java
+++ src/model/Individual.java
@@ -1,10 +1,13 @@
 package model;
 
+import java.awt.image.BufferedImage;
+import java.awt.image.DataBufferByte;
 import java.util.ArrayList;
-import java.util.concurrent.ThreadLocalRandom;
+import java.util.Comparator;
 
-public class Individual {
+public class Individual{
 	private ArrayList<Circle> chromossome;
+	private double fitness;
 	
 	public Individual() {
 		chromossome = new ArrayList<Circle>();
@@ -15,12 +18,25 @@ public class Individual {
 		}
 	}
 	
+	public void setFitness(BufferedImage img, BufferedImage originalImg) {
+		int fitness = 0, subtraction;
+		byte[] imgPixels = ((DataBufferByte) img.getRaster().getDataBuffer()).getData(); //get pixel arrays
+		byte[] originalPixels = ((DataBufferByte) originalImg.getRaster().getDataBuffer()).getData();
+		for(int i = 0; i < imgPixels.length; i++) { //iterate over arrays and subtract equivalent pixels and sum the 
+			subtraction = Math.abs(imgPixels[i] - originalPixels[i]);//get the absolute value of the subtraction 
+			fitness += subtraction;//keep the result into a variable 
+		}
+		this.fitness = fitness/imgPixels.length;
+	}
 	
 	public ArrayList<Circle> getChromossome() {
 		return chromossome;
 	}
 	public void setChromossome(ArrayList<Circle> chromossome) {
 		this.chromossome = chromossome;
+	}
+	public double getFitness() {
+		return this.fitness;
 	}
 	
 	
